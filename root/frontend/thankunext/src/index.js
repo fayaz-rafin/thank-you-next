d64462bebf958eda1da1ae399a44f9eb739e236b
fayaz-rafin <fayaz.rafin@gmail.com>
2022-04-30 02:01:13 -0400

    Title component
    Added a title component

--- root/frontend/thankunext/src/index.js
+++ root/frontend/thankunext/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Title from './title';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Title />
   </React.StrictMode>
 );
 
---
+++ root/frontend/thankunext/src/title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title () {
+  return (
+      <div className="Title">
+        <h1>Thank U, Next</h1>
+        </div>
+    );
+}
+
+export default Title;
